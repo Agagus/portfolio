c45a9e6a52380d7b1a39b1a5f64a079f5b0f851d
Agagus <agus.zanetti4@gmail.com>
2023-06-07 09:57:55 -0300

    projects page design

--- pages/projects.js
+++ pages/projects.js
@@ -2,8 +2,10 @@ import Head from "next/head";
 import React from "react";
 import Container from "./components/Container";
 import starWars from "../star-wars.png";
+import countries from "../pi-countries.png";
 import Image from "next/image";
-
+import Link from "next/link";
+//https://challenge-front-alpha.vercel.app/
 const projects = () => {
   return (
     <>
@@ -11,14 +13,45 @@ const projects = () => {
         <title>Agustina - Proyectos</title>
       </Head>
       <Container>
-        <div className=" pb-16 md:p-12">
-          <h3 className="text-center font-mono text-xl md:text-2xl">
+        <div className=" pb-16 md:p-12 flex flex-col items-center">
+          <h3 className="text-center font-mono text-xl md:hidden">
             MIS PROYECTOS
           </h3>
-          {/* (star-wars, pi, sole rubio, pf) */}
-          <Image src={starWars} alt="Star-wars project" className=" py-2" />
-          <Image src={starWars} alt="Star-wars project" className=" py-2" />
-          <Image src={starWars} alt="Star-wars project" className=" py-2" />
+          <div className="sm:grid sm:grid-cols-2 md:gap-8 gap-4 max-w-4xl">
+            {/* (star-wars, pi, sole rubio, pf) */}
+            <Link href="https://pi-vzqn.vercel.app/" target="_blank">
+              <Image
+                src={countries}
+                alt="Countries project"
+                className=" py-2 sm:py-0 transition-all duration-500 hover:scale-105 hover:cursor-pointer"
+              />
+              <p className="text-center text-sm pb-2 sm:pt-2">App países</p>
+            </Link>
+            <Link
+              href="https://challenge-front-alpha.vercel.app/"
+              target="_blank"
+            >
+              <Image
+                src={starWars}
+                alt="Star-wars project"
+                className="py-2 sm:py-0"
+              />
+              <p className="text-center text-sm pb-2 sm:pt-2">
+                Start-wars battle app
+              </p>
+            </Link>
+            <Link
+              href="https://challenge-front-alpha.vercel.app/"
+              target="_blank"
+            >
+              <Image
+                src={starWars}
+                alt="Star-wars project"
+                className=" py-2 sm:py-0"
+              />
+              <p className="text-center text-sm pb-2 sm:pt-2">App países</p>
+            </Link>
+          </div>
         </div>
       </Container>
     </>
