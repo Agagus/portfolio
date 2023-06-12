9fd7fe2da486faf5cdeb7d215093e51cf81d01e2
Agagus <agus.zanetti4@gmail.com>
2023-06-12 20:00:25 -0300

    footer links

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,22 +2,39 @@ import React from "react";
 import { AiOutlineWhatsApp, AiFillLinkedin } from "react-icons/ai";
 import { VscGithubAlt } from "react-icons/vsc";
 import { FiMail } from "react-icons/fi";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <div className="w-full h-16 fixed bottom-0 left-0 flex flex-row gap-4 justify-center items-center transition-all bg-[rgb(13,5,2)] bg-opacity-50 md:flex-row md:fixed">
-      <div className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out] ">
+      <Link
+        href="https://wa.link/w9i7cb"
+        target="_blank"
+        className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out] "
+      >
         <AiOutlineWhatsApp />
-      </div>
-      <div className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out]">
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/agustina-zanetti-4b7a67204/"
+        target="_blank"
+        className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out]"
+      >
         <AiFillLinkedin />
-      </div>
-      <div className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out]">
+      </Link>
+      <Link
+        href="https://github.com/Agagus"
+        target="_blank"
+        className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out]"
+      >
         <VscGithubAlt />
-      </div>
-      <div className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out]">
+      </Link>
+      <Link
+        href="mailto:agus.zanett@gmail.com"
+        target="_blank"
+        className="p-2 bg-[rgb(23,9,3)] rounded-full hover:cursor-pointer hover:animate-[ping_1s_ease-out]"
+      >
         <FiMail />
-      </div>
+      </Link>
     </div>
   );
 };
